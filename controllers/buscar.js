2ea8ffa0ee300a6a9717a095765c6fc905d6f235
Sergio Mejia <sdmejiac@correo.udistrital.edu.co>
2023-02-04 21:38:27 -0500

    Buscar por otros argumentos en todas las colecciones

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -1,5 +1,5 @@
 const { response } = require("express");
-const { Usuario } = require("../models");
+const { Usuario, Product, Category } = require("../models");
 const { ObjectId } = require("mongoose").Types;
 
 
@@ -15,14 +15,67 @@ const buscarUsuarios = async( termino = '', res = response) => {
 
     if( esMongoId ){
         const usuario = await Usuario.findById( termino );
-
-        res.json({
+        //return para no ejecutar nada fuera del if
+        return res.json({
             //Pregunta por el usuario, si existe envia el arreglo con el usuario si no, envia arreglo vacio
             results: (usuario) ? [usuario] : []
         })
     }
+
+    //Expresion regular -> Hacer ****insensible**** a minusculas y mayusculas
+    const regex = new RegExp( termino, 'i')
+
+    const usuarios = await Usuario.find({ 
+        $or:[{ name: regex }, { email: regex }],
+        $and: [{ state: true}]
+    });
+    res.json({
+        results: usuarios
+    })
 }
 
+
+const buscarProductos = async( termino = '', res = response) => {
+    const esMongoId = ObjectId.isValid( termino );
+
+    if( esMongoId ){
+        const producto = await Product.findById( termino )
+                                .populate( 'categoria', 'name' );
+        return res.json({
+            results: (producto) ? [producto] : []
+        })
+    }
+
+    const regex = new RegExp( termino, 'i' )
+
+    const productos = await Product.find({ name: regex, estado: true })
+                            .populate( 'categoria', 'name' );
+
+
+    res.json({
+        result: productos
+    });
+}
+
+
+const buscarCategories = async( termino = '', res = response) => {
+    const esMongoId = ObjectId.isValid( termino );
+
+    if( esMongoId ){
+        const categoria = await Category.findById( termino );
+        return res.json({
+            result: ( categoria ) ? [ categoria ] : []
+        })
+    }
+
+    const regex = new RegExp( termino, 'i');
+
+    const categories = await Category.find({ name: regex, estado: true })
+
+    res.json({
+        result: categories
+    })
+}
 
 const buscar = (req, res = response) => {
 
@@ -38,10 +91,13 @@ const buscar = (req, res = response) => {
         case 'users':
             buscarUsuarios( termino, res )
         break;
-        case 'products':
-        break;
-        case 'categories':
 
+        case 'products':
+            buscarProductos( termino, res )
+        break;
+
+        case 'categories':
+            buscarCategories( termino, res )
         break;
 
         default:
