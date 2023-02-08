1d7cf953f1451db0e1ef02863335085b0c2ef353
Sergio Mejia <sdmejiac@gmail.com>
2023-02-07 21:01:56 -0500

    Borrar archivos del servidor

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,4 +1,5 @@
-
+const path = require('path');
+const fs = require('fs');
 const { response } = require("express");
 const { subirArchivo } = require("../helpers");
 const { Usuario, Product }  = require('../models')
@@ -43,6 +44,18 @@ const actualizarImagen = async( req, res = response ) => {
         default:
             return res.status(500).json({msg: `No validado`});
     }
+
+
+    //Eliminar imagenes previas 
+    //Si la propiedad img existe en el servidor
+    if( modelo.img ){
+        //Borarr imagen del servidor
+        const pathImage = path.join( __dirname, '../uploads', coleccion, modelo.img);
+        if( fs.existsSync( pathImage )){
+            fs.unlinkSync( pathImage );
+        }
+    }
+
 
     const nombre = await subirArchivo( req.files, undefined , coleccion);
     modelo.img = nombre;
